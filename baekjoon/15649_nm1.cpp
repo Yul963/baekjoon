957da15f2b0c7254db67b8c6a6b471dc83bc9bb4
Yul963 <dbfrb9786@gmail.com>
2024-02-18 21:48:07 +0900

    .

---
+++ baekjoon/15649_nm1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+int n, m;
+std::pair<int, bool> a[9] = { {0, 0} };
+
+void dfs(int cnt)
+{
+    if (cnt == m){
+        for (int i = 0; i < m; i++)
+            std::cout << a[i].first << ' ';
+        std::cout << '\n';
+        return;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (!a[i].second) {
+            a[i].second = true;
+            a[cnt].first = i;
+            dfs(cnt + 1);
+            a[i].second = false;
+        }
+    }
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	std::cin >> n >> m;
+    dfs(0);
+
+	return 0;
+}
